d5ba06f462601677cd7d2afdcc37395a9a1e29ac
ramita-sardana <ramita.gambhir@gmail.com>
2025-12-27 11:26:58 +0530

    address bean

--- demo-ramita/src/main/java/com/example/demo/beans/Address.java
+++ demo-ramita/src/main/java/com/example/demo/beans/Address.java
@@ -1,4 +1,38 @@
 package com.example.demo.beans;
 
 public class Address {
+	
+	private String street;
+	
+	private String city;
+
+	/**
+	 * @return the street
+	 */
+	public String getStreet() {
+		return street;
+	}
+
+	/**
+	 * @param street the street to set
+	 */
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	/**
+	 * @return the city
+	 */
+	public String getCity() {
+		return city;
+	}
+
+	/**
+	 * @param city the city to set
+	 */
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	
 }
